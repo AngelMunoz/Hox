353bcf24296e04d88ce68caff621027fc351474b
Angel D. Munoz <scythes.lol@gmail.com>
2024-01-18 22:54:56 -0600

    small changes in the example

--- samples/Server/Program.fs
+++ samples/Server/Program.fs
@@ -32,18 +32,16 @@ module Extensions =
 
       ctx.Response.ContentType <- "text/html; charset=utf-8"
 
-      do!
-        Chunked.render view ctx.RequestAborted
-        |> TaskSeq.iterAsync (fun chunk -> task {
-          do! writer.WriteAsync(chunk)
-          do! writer.FlushAsync()
-        })
+      for chunk in Chunked.render view ctx.RequestAborted do
+        do! writer.WriteAsync(chunk)
+        do! writer.FlushAsync()
     }
 
     [<Extension>]
-    static member inline renderView(ctx: HttpContext, view: Node) =
-      Builder.Task.render view ctx.RequestAborted
-      |> Task.map (fun result -> Results.Text(result, "text/html"))
+    static member inline renderView(ctx: HttpContext, view: Node) = task {
+      let! result = Builder.Task.render view ctx.RequestAborted
+      return Results.Text(result, "text/html")
+    }
 
 type Layout =
   static member inline Default(content: Node, ?head: Node, ?scripts: Node) =
@@ -57,16 +55,14 @@ type Layout =
         El "meta[charset=utf-8]"
         El "meta[name=viewport][content=width=device-width, initial-scale=1.0]"
         El "title" |> Text "Hello World!"
-        // bulma css link tag
         El
           "link[rel=stylesheet][href=https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css]"
         head
-        // htmx script tag
-        El "script[src=https://unpkg.com/htmx.org@1]"
       ]
       El "body"
       |> Children [
         content
+        El "script[src=https://unpkg.com/htmx.org@1]"
         scripts
       ]
 
@@ -98,12 +94,29 @@ let renderTodos (http: HttpClient) = vTask {
   return El "ul" |> Children todoItems
 }
 
-let inline index (ctx: HttpContext) = task {
-  return! ctx.renderView (Layout.Default(El "h1" |> Text "Hello World!"))
+let inline index (ctx: HttpContext) (factory: IHttpClientFactory) = task {
+  let http = factory.CreateClient()
+  let todos = renderTodos http
+
+  return!
+    ctx.renderView (
+      Layout.Default(
+        Fragment [
+          El "h1" |> Text "Hello World!"
+          AwaitChildren(
+            taskSeq {
+              for i in 1..10 do
+                yield El "p" |> Text($"Paragraph %d{i}")
+            }
+          )
+          vTaskEl todos
+        ]
+      )
+    )
 }
 
 let inline streamed (ctx: HttpContext) (factory: IHttpClientFactory) = taskUnit {
-  let http = factory.CreateClient("Streamed")
+  let http = factory.CreateClient()
   let todos = renderTodos http
 
   return!
@@ -126,10 +139,11 @@ let inline streamed (ctx: HttpContext) (factory: IHttpClientFactory) = taskUnit 
 [<EntryPoint>]
 let main args =
   let builder = WebApplication.CreateBuilder(args)
-  builder.Services.AddHttpClient("Streamed") |> ignore
+  builder.Services.AddHttpClient() |> ignore
   let app = builder.Build()
 
-  app.MapGet("/", Func<HttpContext, Task<IResult>>(index)) |> ignore
+  app.MapGet("/", Func<HttpContext, IHttpClientFactory, Task<IResult>>(index))
+  |> ignore
 
   app.MapGet("/streamed", Func<HttpContext, IHttpClientFactory, Task>(streamed))
   |> ignore
