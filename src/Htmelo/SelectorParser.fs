3b2b6e898c4c3ed0304671d0a778adfc289d9056
Angel D. Munoz <scythes.lol@gmail.com>
2024-01-20 15:41:59 -0600

    Update namespaces and  calls

--- src/Htmelo/SelectorParser.fs
+++ src/Htmelo/SelectorParser.fs
@@ -1,111 +1,120 @@
-namespace Htmelo
+[<RequireQualifiedAccess>]
+module Htmelo.Parsers
 
 open System.Collections.Immutable
 
+open FParsec
+
+open Htmelo.Core
+
 [<Struct>]
-type SelectorValue =
+type private SelectorValue =
   | Id of id: string
   | Class of classes: string
-  | Attribute of attributes: Htmelo.Attribute
+  | Attribute of attributes: HAttribute
 
-module Parsers =
-  open FParsec
+let private tagName: Parser<string, unit> =
+  let options =
+    IdentifierOptions(
+      isAsciiIdStart = isAsciiLetter,
+      isAsciiIdContinue = fun ch -> isAsciiLetter ch || isDigit ch || ch = '-'
+    )
 
-  let tagName: Parser<string, unit> =
-    let options =
-      IdentifierOptions(
-        isAsciiIdStart = isAsciiLetter,
-        isAsciiIdContinue = fun ch -> isAsciiLetter ch || isDigit ch || ch = '-'
-      )
+  identifier(options)
 
-    identifier(options)
+let private pId: Parser<SelectorValue, unit> =
+  let value = satisfy(fun ch -> ch <> '#' && ch <> '.' && ch <> '[')
 
-  let pId: Parser<SelectorValue, unit> =
-    let value = satisfy(fun ch -> ch <> '#' && ch <> '.' && ch <> '[')
-    pchar '#' >>. manyChars value >>= (fun id -> preturn(Id id))
+  pchar '#' >>. manyChars value .>> unicodeSpaces >>= (fun id -> preturn(Id id))
 
-  let pClass: Parser<SelectorValue, unit> =
-    pchar '.' >>. manyChars(letter <|> digit <|> pchar '-')
-    >>= fun cls -> preturn(Class cls)
+let private pClass: Parser<SelectorValue, unit> =
+  let avoid =
+    noneOf [
+      ' '
+      '\t'
+      '\n'
+      '\r'
+      '['
+      '.'
+      '#'
+    ]
 
-  let pAttribute: Parser<SelectorValue, unit> =
-    let name = manyChars(letter <|> digit <|> pchar '-')
-    let eq = pchar '='
+  pchar '.' >>. manyChars(letter <|> digit <|> pchar '-' <|> avoid)
+  .>> unicodeSpaces
+  >>= fun cls -> preturn(Class cls)
 
-    let value = manyChars(satisfy(fun ch -> ch <> ']'))
+let private pAttribute: Parser<SelectorValue, unit> =
+  let name = manyChars(letter <|> digit <|> pchar '-')
+  let eq = pchar '='
 
-    pchar '[' >>. name .>> eq .>>. value .>> unicodeSpaces .>> pchar ']'
-    >>= fun (name, value) -> preturn(Attribute { name = name; value = value })
+  let value = manyChars(satisfy(fun ch -> ch <> ']'))
 
-  let pSelector: Parser<Element, unit> =
-    tagName .>>. many(attempt pClass <|> attempt pAttribute <|> attempt pId)
-    >>= fun (tag, values) ->
-      let dcBuilder = ImmutableDictionary.CreateBuilder<string, AttributeNode>()
+  pchar '[' >>. name .>> eq .>>. value
+  .>> unicodeSpaces
+  .>> pchar ']'
+  .>> unicodeSpaces
+  >>= fun (name, value) -> preturn(Attribute { name = name; value = value })
 
-      for attributes in values do
-        match attributes with
-        | Attribute { name = "id"; value = value }
-        | Id value ->
+let private pSelector: Parser<Element, unit> =
+  tagName .>> unicodeSpaces
+  .>>. many(attempt pClass <|> attempt pAttribute <|> attempt pId)
+  .>> unicodeSpaces
+  >>= fun (tag, values) ->
+    let dcBuilder = ImmutableDictionary.CreateBuilder<string, AttributeNode>()
+
+    for attributes in values do
+      match attributes with
+      | Attribute { name = "id"; value = value }
+      | Id value ->
+        dcBuilder.Add(
+          "id",
+          AttributeNode.Attribute { name = "id"; value = value.Trim() }
+        )
+      | Attribute { name = "class"; value = value }
+      | Class value ->
+        match dcBuilder.TryGetValue("class") with
+        | true, AttributeNode.Attribute { name = "class"; value = classes } ->
+          dcBuilder.Remove("class") |> ignore
+
           dcBuilder.Add(
-            "id",
-            AttributeNode.Attribute { name = "id"; value = value }
+            "class",
+            AttributeNode.Attribute {
+              name = "class"
+              value = $"%s{classes} %s{value}"
+            }
           )
-        | Attribute { name = "class"; value = value }
-        | Class value ->
-          match dcBuilder.TryGetValue("class") with
-          | true, AttributeNode.Attribute { name = "class"; value = classes } ->
-            dcBuilder.Remove("class") |> ignore
+        | false, _ ->
+          dcBuilder.Add(
+            "class",
+            AttributeNode.Attribute { name = "class"; value = value }
+          )
+        | _, _ -> ()
+      | Attribute attribute ->
+        match dcBuilder.TryGetValue(attribute.name) with
+        | true, AttributeNode.Attribute { name = key; value = value } ->
+          dcBuilder.Remove(key) |> ignore
 
-            dcBuilder.Add(
-              "class",
-              AttributeNode.Attribute {
-                name = "class"
-                value = $"%s{classes} %s{value}"
-              }
-            )
-          | false, _ ->
-            dcBuilder.Add(
-              "class",
-              AttributeNode.Attribute { name = "class"; value = value }
-            )
-          | _, _ -> ()
-        | Attribute attribute ->
-          match dcBuilder.TryGetValue(attribute.name) with
-          | true, AttributeNode.Attribute { name = key; value = value } ->
-            dcBuilder.Remove(key) |> ignore
+          dcBuilder.Add(
+            key,
+            AttributeNode.Attribute {
+              name = key
+              value = $"%s{value} %s{attribute.value}"
+            }
+          )
+        | false, _ ->
+          dcBuilder.Add(attribute.name, AttributeNode.Attribute attribute)
+        | _, _ -> ()
 
-            dcBuilder.Add(
-              key,
-              AttributeNode.Attribute {
-                name = key
-                value = $"%s{value} %s{attribute.value}"
-              }
-            )
-          | false, _ ->
-            dcBuilder.Add(attribute.name, AttributeNode.Attribute attribute)
-          | _, _ -> ()
+    preturn {
+      tag = tag
+      attributes =
+        dcBuilder.ToImmutableList()
+        |> Seq.map(fun pair -> pair.Value)
+        |> Seq.toList
+      children = []
+    }
 
-      preturn {
-        tag = tag
-        attributes =
-          dcBuilder.ToImmutableList()
-          |> Seq.map(fun pair -> pair.Value)
-          |> Seq.toList
-        children = []
-      }
-
-  let selector(selector: string) =
-    match run pSelector selector with
-    | Success(result, _, _) -> result
-    | Failure(origin, err, _) -> failwith $"Failed to parse '{origin}': {err}"
-
-  let selectorResult(selector: string) =
-    match run pSelector selector with
-    | Success(result, _, _) -> Result.Ok result
-    | Failure(origin, err, _) ->
-      Result.Error($"Failed to parse '{origin}': {err}")
-
-  let trySelector(selector: string) =
-    match run pSelector selector with
-    | Success(result, _, _) -> Some result
-    | Failure(_, _, _) -> None
+let selector(selector: string) =
+  match run pSelector selector with
+  | Success(result, _, _) -> result
+  | Failure(origin, err, _) -> failwith $"Failed to parse '{origin}': {err}"
