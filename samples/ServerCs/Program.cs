6272167241bc4c469580f347517f1add54ccabe1
Angel D. Munoz <scythes.lol@gmail.com>
2024-01-21 15:42:35 -0600

    update C# sample

--- samples/ServerCs/Program.cs
+++ samples/ServerCs/Program.cs
@@ -1,4 +1,7 @@
 using Htmelo.Core;
+
+using System.Diagnostics;
+
 using static Htmelo.NodeBuilder;
 using static Htmelo.Rendering;
 
@@ -7,73 +10,86 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddLogging();
 var app = builder.Build();
 
-app.MapGet("/", async (HttpContext ctx) =>
+app.MapGet("/", (HttpContext ctx) =>
 {
-  var node =
-      Layout.Default(
-        h("h1", text("Hello World!")),
-        h("p", text("This is a paragraph."))
-      );
+    var node =
+        Layout.Default(
+          h("h1", text("Hello World!")),
+          h("p", text("This is a paragraph."))
+        );
 
-  // return await ctx.RenderView(node);
-  await ctx.StreamView(node);
+    // return await ctx.RenderView(node);
+    return node.ToStreamedResult();
 });
 
 app.Run();
 
 static class Layout
 {
-  public record DefaultLayoutArgs(Node? Head, Node? Scripts);
+    public record DefaultLayoutArgs(Node? Head, Node? Scripts);
 
-  public static Node Default(DefaultLayoutArgs args, params Node[] children) =>
-    h(
-      "html[lang=en].sl-theme-light",
+    public static Node Default(DefaultLayoutArgs args, params Node[] children) =>
       h(
-        "head",
-        h("meta[charset=utf-8]"),
-        h(@"meta[name=viewport]
+        "html[lang=en].sl-theme-light",
+        h(
+          "head",
+          h("meta[charset=utf-8]"),
+          h(@"meta[name=viewport]
                  [content=width=device-width, initial-scale=1.0]
             "),
-        h("title", text("Htmelo")),
-        h(@"link[rel=stylesheet]
+          h("title", text("Htmelo")),
+          h(@"link[rel=stylesheet]
                  [href=https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/themes/light.css]
                  [media=(prefers-color-scheme:light)]"),
-        h(@"link[rel=stylesheet]
+          h(@"link[rel=stylesheet]
                  [href=https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/themes/dark.css]
                  [media=(prefers-color-scheme:dark)]
                  [onload=document.documentElement.classList.add('sl-theme-dark');]"),
-        args.Head ?? fragment([])
-      ),
-      h(
-        "body",
-        fragment(children),
-        h(@"script[type=module]
+          args.Head ?? fragment([])
+        ),
+        h(
+          "body",
+          fragment(children),
+          h(@"script[type=module]
                    [src=https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/shoelace-autoloader.js]"),
-        args.Scripts ?? fragment([])
-      )
-    );
+          args.Scripts ?? fragment([])
+        )
+      );
 
-  public static Node Default(params Node[] children) =>
-    Default(new DefaultLayoutArgs(null, null), children);
+    public static Node Default(params Node[] children) =>
+      Default(new DefaultLayoutArgs(null, null), children);
 }
 
-
-static class HttpContextExtensions
+public static class ResultsExtensions
 {
-  public static async Task<IResult> RenderView(this HttpContext ctx, Node node)
-  {
-    var content = await Builder.ValueTask.render(node, ctx.RequestAborted);
-
-    return Results.Text(content, "text/html; charset=utf-8");
-  }
-
-  public static async Task StreamView(this HttpContext context, Node node)
-  {
-    context.Response.ContentType = "text/html; charset=utf-8";
-    await foreach (var item in Chunked.render(node, context.RequestAborted))
+    public static IResult ToStreamedResult(this Node node) => new HtmeloStreamedResult(node);
+    public static async Task<IResult> ToTextResult(this Node node, CancellationToken cancellationToken = default)
     {
-      await context.Response.BodyWriter.WriteAsync(new ReadOnlyMemory<byte>(System.Text.Encoding.UTF8.GetBytes(item)), context.RequestAborted);
-      await context.Response.BodyWriter.FlushAsync(context.RequestAborted);
+        try
+        {
+            var result = await Builder.ValueTask.render(node, cancellationToken);
+            return Results.Text(result, "text/html; charset=utf-8");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            return Results.Problem("Server Error", statusCode: 500);
+        }
     }
-  }
+}
+
+public class HtmeloStreamedResult(Node node) : IResult
+{
+
+    public async Task ExecuteAsync(HttpContext httpContext)
+    {
+        httpContext.Response.ContentType = "text/html; charset=utf-8";
+        await httpContext.Response.StartAsync(httpContext.RequestAborted);
+        await foreach (var item in Chunked.render(node, httpContext.RequestAborted))
+        {
+            await httpContext.Response.BodyWriter.WriteAsync(new ReadOnlyMemory<byte>(System.Text.Encoding.UTF8.GetBytes(item)), httpContext.RequestAborted);
+            await httpContext.Response.BodyWriter.FlushAsync(httpContext.RequestAborted);
+        }
+        await httpContext.Response.CompleteAsync();
+    }
 }
