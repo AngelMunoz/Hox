344d2f29b13bd789668aed2f2afe3c9b366fb712
Angel D. Munoz <scythes.lol@gmail.com>
2024-01-26 08:17:27 -0600

    add ToC parsing and markdown extraction

---
+++ src/Hox.Docs/ToC.fs
@@ -0,0 +1,81 @@
+module Hox.Docs.Toc
+
+
+open System
+open System.IO
+
+open Thoth.Json.Net
+
+open FSharp.Control
+open IcedTasks
+open Hox.Markdown
+open System.Threading
+
+module Decoders =
+
+  let dateOnly: Decoder<DateOnly> =
+    fun path value ->
+      if Decode.Helpers.isString value then
+        let unboxedval = unbox<string> value
+
+        match DateOnly.TryParse(unboxedval) with
+        | true, date -> Ok date
+        | false, _ -> Error(path, BadPrimitive("DateOnly", value))
+      else
+        Error(path, BadPrimitive("DateOnly", value))
+
+  let dateOnlyExact(format: string) : Decoder<DateOnly> =
+    fun path value ->
+      if Decode.Helpers.isString value then
+        let unboxedval = unbox<string> value
+
+        match DateOnly.TryParseExact(unboxedval, format) with
+        | true, date -> Ok date
+        | false, _ -> Error(path, BadPrimitive("DateOnly", value))
+      else
+        Error(path, BadPrimitive("DateOnly", value))
+
+type EntryMetadata = {
+  title: string
+  file: string
+  author: string
+  contributors: string list
+  summary: string
+  updated: DateOnly option
+  category: string option
+}
+
+type EntryMetadata with
+
+  static member Decode: Decoder<EntryMetadata> =
+    Decode.object(fun ob -> {
+      title = ob.Required.Field "title" Decode.string
+      file = ob.Required.Field "file" Decode.string
+      author = ob.Required.Field "author" Decode.string
+      contributors =
+        ob.Required.Field "contributors" (Decode.list Decode.string)
+      summary = ob.Required.Field "summary" Decode.string
+      updated = ob.Optional.Field "updated" Decoders.dateOnly
+      category = ob.Optional.Field "category" Decode.string
+    })
+
+let getMetadata() = task {
+  let! content = File.ReadAllTextAsync("markdown/toc.json")
+
+  match Decode.fromString (Decode.list(EntryMetadata.Decode)) content with
+  | Ok entries -> return entries
+  | Error err -> return failwith $"Error decoding metadata: %A{err}"
+}
+
+type ToC =
+  static member getContent(?cancellationToken: CancellationToken) =
+    let token = defaultArg cancellationToken CancellationToken.None
+
+    taskSeq {
+      let! metadata = getMetadata()
+
+      for entry in metadata do
+        token.ThrowIfCancellationRequested()
+        let! content = Html.ofMarkdownFile $"markdown/{entry.file}" token
+        entry, content
+    }
