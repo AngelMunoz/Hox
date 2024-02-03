ae360eba67dbcddebab96931044ba73624f94153
Angel D. Munoz <scythes.lol@gmail.com>
2024-02-03 01:50:24 -0600

    add missing API

--- src/Hox/DSL.fs
+++ src/Hox/DSL.fs
@@ -520,6 +520,8 @@ type NodeDsl =
   static member inline fragment(nodes: IAsyncEnumerable<Node>) =
     AsyncSeqNode nodes
 
+  static member inline empty = Fragment(LinkedList())
+
   static member inline attribute(name: string, value: string) =
     Attribute { name = name; value = value }
 
